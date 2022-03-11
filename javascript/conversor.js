2b239809550efcb8121a79c01e355f9b856ec98e
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-03-11 16:31:26 -0300

    V.0.1 - alfa

---
+++ javascript/conversor.js
@@ -0,0 +1,13 @@
+const onerem = 16;
+const clear = 0;
+const texto = "O valor em rem é: ";
+function converter() {
+    var pxvalue = document.getElementById('cnv-input').value;
+    var valueconvert = Number(pxvalue * onerem);
+    document.getElementById("cvn-result").innerHTML =  valueconvert;
+}
+    function limpar(){
+        document.getElementById("cvn-result").innerHTML = clear;
+        document.location.reload(true);
+
+}
---
+++ javascript/test.js
@@ -0,0 +1,49 @@
+/*
+function firstalert(user){
+    console.log("Olá, " + user);
+    
+    }
+    firstalert("Petrus"); //"olá mundo"
+
+
+var test = "douglas";
+
+function firstif(){
+    
+    if(test === "name"){
+        console.log("Sim você está cadastrado");
+    }
+    else{
+        console.log("Crie uma conta, você não está cadastrado!");
+    }
+}
+
+
+    */
+
+
+
+var onerem = 16;
+var onepx = 0.06;
+
+    var converterpxrem = 1 * onerem;
+    var converterrempx = 1 * onepx;
+
+console.log(converterpxrem) 
+console.log(converterrempx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
