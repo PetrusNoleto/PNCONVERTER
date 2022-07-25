fe7e09b408d30f0c906a73632445b356c635191d
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-07-25 18:46:13 -0300

    up

--- javascript/conversor.js
+++ javascript/conversor.js
@@ -6,22 +6,22 @@
         const _fontSizeDefault = 16
         let $fontSize = document.querySelector(".getfontsize").value
         let $pxvalue = document.querySelector(".getPixels").value
-       
     
+            if($fontSize == _fontSizeDefault){
+                let _calcFontSizeDefault = Number($pxvalue / _fontSizeDefault) 
+                let _result = _calcFontSizeDefault
+                document.querySelector(".resulttxt").textContent = _result
 
-        if($fontSize == _fontSizeDefault){
-            let _calcFontSizeDefault = Number($pxvalue / _fontSizeDefault) 
-            let _result = _calcFontSizeDefault
-            document.querySelector(".resulttxt").textContent = _result
-
-        }
-         else($fontSize != _fontSizeDefault) 
-            let _remcalc = Number($pxvalue / $fontSize)
-            document.querySelector(".resulttxt").textContent = (_remcalc)
-
+            }
+            else($fontSize != _fontSizeDefault) 
+                let _remcalc = Number($pxvalue / $fontSize)
+                document.querySelector(".resulttxt").textContent = (_remcalc)
         
-
-    
+            
+            if($fontSize, $pxvalue == 0){
+                document.querySelector(".resulttxt").textContent = ("")
+            }
+        
 
 
     
