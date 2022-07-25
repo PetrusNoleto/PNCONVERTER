c69146ae109522c4e3b40600719fe63a024da679
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-07-25 17:49:35 -0300

    teste

--- javascript/conversor.js
+++ javascript/conversor.js
@@ -1,10 +1,25 @@
-const onerem = 0.0625;
-const texto = "O valor em rem é: ";
-const $convbtn = document.querySelector(".confirmbtn")
 
-$convbtn.addEventListener("click", function converter(){
 
-    let pxvalue = document.querySelector(".pxvalue").value
-    let valueconvert = Number(pxvalue * onerem)
-    document.querySelector(".resultado").textContent =  valueconvert
-} )
+
+
+
+
+    const $button = document.querySelector(".btn-converter")
+    $button.addEventListener("click", converter())
+
+
+    function converter(){
+    const _fontSizeDefault = 16
+    let $fontSize = document.querySelector(".getfontsize").value
+    let $pxvalue = document.querySelector(".getPixels").value
+    let $calcresult = document.querySelector(".resulttxt").value 
+    
+    let _remcalc = Number($pxvalue / $fontSize)
+    
+    
+
+
+    console.log(_remcalc)
+
+
+}
