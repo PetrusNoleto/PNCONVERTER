8d549aa3cdb6f85cada535b709e379c1d43a731e
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-07-25 18:30:42 -0300

    Update conversor.js

--- javascript/conversor.js
+++ javascript/conversor.js
@@ -1,25 +1,30 @@
-
-
-
-
-
-
+    const _fontSizeDefault = 16
     const $button = document.querySelector(".btn-converter")
     $button.addEventListener("click", converter())
 
 
     function converter(){
-    const _fontSizeDefault = 16
+    
     let $fontSize = document.querySelector(".getfontsize").value
     let $pxvalue = document.querySelector(".getPixels").value
     let $calcresult = document.querySelector(".resulttxt").value 
+    let txt = "Rem";
     
-    let _remcalc = Number($pxvalue / $fontSize)
-    
+
+        if($fontSize == _fontSizeDefault){
+            let _calcFontSizeDefault = Number($pxvalue / _fontSizeDefault) 
+            let _result =  _calcFontSizeDefault
+
+
+        } else{
+            let _remcalc = Number($pxvalue / $fontSize)
+            
+
+        }
+
     
 
 
-    console.log(_remcalc)
-
+    
 
 }
