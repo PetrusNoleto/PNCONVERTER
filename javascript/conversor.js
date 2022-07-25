1df9709692a92cb984504ec8c40b80c886c03bf8
PETRUS <88741218+PetrusNoleto@users.noreply.github.com>
2022-07-25 18:41:19 -0300

    update

--- javascript/conversor.js
+++ javascript/conversor.js
@@ -1,26 +1,25 @@
-    const _fontSizeDefault = 16
+    
     const $button = document.querySelector(".btn-converter")
     $button.addEventListener("click", converter())
-
-
-    function converter(){
     
-    let $fontSize = document.querySelector(".getfontsize").value
-    let $pxvalue = document.querySelector(".getPixels").value
-    let $calcresult = document.querySelector(".resulttxt").value 
-    let txt = "Rem";
+    function converter(){
+        const _fontSizeDefault = 16
+        let $fontSize = document.querySelector(".getfontsize").value
+        let $pxvalue = document.querySelector(".getPixels").value
+       
     
 
         if($fontSize == _fontSizeDefault){
             let _calcFontSizeDefault = Number($pxvalue / _fontSizeDefault) 
-            let _result =  _calcFontSizeDefault
-
-
-        } else{
-            let _remcalc = Number($pxvalue / $fontSize)
-            
+            let _result = _calcFontSizeDefault
+            document.querySelector(".resulttxt").textContent = _result
 
         }
+         else($fontSize != _fontSizeDefault) 
+            let _remcalc = Number($pxvalue / $fontSize)
+            document.querySelector(".resulttxt").textContent = (_remcalc)
+
+        
 
     
 
