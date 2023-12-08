6661f32a3f794c4c731f94e790e20d314b05c640
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2023-12-08 13:47:35 -0300

    v2.0 - beta test
    responsive : no
    accessibility : no
    Factored: no

---
+++ scripts/pxtoremconverter.js
@@ -0,0 +1,22 @@
+//    const $button = document.querySelector(".btn-converter")
+//     $button.addEventListener("click", converter())    
+function conversorPxToRem(){
+    const fontSizeDefault = 16
+    let $PxToRemfontSize = document.querySelector(".conversorPxToRemGetFontSize").value
+    let $PxToRemPxvalue = document.querySelector(".conversorPxToRemGetPixels").value
+    let $getClassForResult = document.querySelector(".mainConversorResult").classList    
+    if($PxToRemfontSize == fontSizeDefault){
+            let _calcFontSizeDefault = Number($PxToRemPxvalue / fontSizeDefault) 
+            let _result = _calcFontSizeDefault
+            $getClassForResult.remove(".hidden")
+            document.querySelector(".conversorPxToRemResult").textContent = (`${_result}rem`)
+        }
+        else($PxToRemfontSize != fontSizeDefault) 
+            let _remcalc = Number($PxToRemPxvalue / $PxToRemfontSize)
+            $getClassForResult.remove("hidden")
+            document.querySelector(".conversorPxToRemResult").textContent = (`${_remcalc}rem`)    
+        if($PxToRemfontSize, $PxToRemPxvalue == 0){
+            $getClassForResult.remove("hidden")
+            document.querySelector(".conversorPxToRemResult").textContent = ("")   
+        }
+}
